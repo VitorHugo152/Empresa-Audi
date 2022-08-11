bccc6b02e9cd911e7baa80941b0d7d3d9d1a36d0
VitorHugo152 <vitorhugo1800@hotmail.com>
2022-08-11 17:55:11 -0300

    Update Empresa Audi.sql

--- Empresa Audi.sql
+++ Empresa Audi.sql
@@ -18,15 +18,7 @@ potencia VARCHAR (4),
 tipo_de_carro VARCHAR (40)
 )
 
-CREATE TABLE Camionetes (
-placa VARCHAR (18),
-nome VARCHAR (38),
-ano DATE,
-modelo VARCHAR (38),
-cor VARCHAR (38),
-potencia VARCHAR (4),
-tipo_de_carro VARCHAR (38)
-)
+
 
 
 INSERT INTO Carros (placa,nome,ano,modelo,cor,potencia,tipo_de_carro)
@@ -36,4 +28,10 @@ INSERT INTO Carros (placa,nome,ano,modelo,cor,potencia,tipo_de_carro)
 VALUES ('12D44','Audi R8','2012-08-07','Audi R8 e-tron','Prata','V10','elétrico')
 
 INSERT INTO SUVS (placa,nome,ano,modelo,cor,potencia,tipo_de_carro)
-VALUES ('11D54','Audi Q3','2011-08-17','','Azul','','')
+VALUES ('11D54','Audi Q3','2011-08-17','Q3 Prestige','Azul','1.4 TSI','gasolina')
+
+
+INSERT INTO SUVS (placa,nome,ano,modelo,cor,potencia,tipo_de_carro)
+VALUES ('13D56','Audi Q4 e-tron','2021-06-19','Q4 40 e-tron','Amarelo','170 cavalos','eletrico')
+
+
